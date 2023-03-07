d746b537cd1fd581e543c807c1273c0628ca4349
Diego <diego.perez.ceb@immune.institute>
2023-03-07 15:03:10 +0100

    prueba

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule as AngularCommon } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { LayoutComponent } from './layout/layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    LayoutComponent
+    LayoutComponent,
+    LoginPageComponent
   ],
   imports: [
     AngularCommon
