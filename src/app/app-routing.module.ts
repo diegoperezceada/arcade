c96dff8f6ce18aa8cfa2ded90547c5da2981dcd6
Diego <diego.perez.ceb@immune.institute>
2023-03-07 16:08:16 +0100

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './common/layout/layout.component';
+import { LoginPageComponent } from './common/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+  },
+  {
+    path: 'login', component: LoginPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
